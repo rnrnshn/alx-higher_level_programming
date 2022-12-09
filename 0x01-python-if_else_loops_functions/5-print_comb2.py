61176352a59d612ed98e6208d560cdf1563102c9
rnrnshn <olymangue@gmail.com>
2022-12-09 16:52:57 +0200

    fix 5-print_comb2.py to pass checks

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i < 10:
-        print("0{}".format(i), end=', ')
+        print("0{}".format(i), sep=',', end=' ')
     else:
-        print("{}".format(i), end=', ')
+        print("{}".format(i), sep=',', end=' ')
