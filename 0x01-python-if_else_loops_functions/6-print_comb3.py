bbf4db8bcc55876b5dd529ba774eb310357648ee
rnrnshn <olymangue@gmail.com>
2022-12-11 00:40:44 +0200

    fix 6-print_comb3.py to pass checks

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(1, 90):
-    if i < int("{0:02d}".format(i)[::-1]):
+   if i < int("{0:02d}".format(i)[::-1]):
         if i < 89:
-            print("{0:02d}".format(i), end=', ')
+            print("{0:02d}, ".format(i), end='')
         else:
-            print("{} \n".format(i))
+            print("{}".format(i))
