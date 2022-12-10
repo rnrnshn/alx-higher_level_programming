0789ed6acd202a7efafb6c79978e8dc0929ac9ce
rnrnshn <olymangue@gmail.com>
2022-12-11 00:06:06 +0200

    add 6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(1, 90):
+    if i < int("{0:02d}".format(i)[::-1]):
+        if i < 89:
+            print(i, end=', ')
+        else:
+            print(i, end=' ')
