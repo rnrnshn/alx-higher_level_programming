c400ec8a142e60a1b7658e5612f26c6aaf07941d
rnrnshn <olymangue@gmail.com>
2023-01-09 22:33:28 +0200

    refactor 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-        last_digit = abs(((number * -1) % 10) * -1)
-    else:
-        last_digit = number % 10
+    last_digit = abs(number) % 10
     print("{}".format(last_digit), end='')
     return last_digit
