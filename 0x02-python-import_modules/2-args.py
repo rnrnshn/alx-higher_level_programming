855164235b27c3e93837e84022d9705782444332
rnrnshn <olymangue@gmail.com>
2023-01-12 01:04:03 +0200

    make the code not execute when imported

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-# This program prints the number pf and the list of its arguments
-import sys
+if __name__ == "__main__":
+    import sys
 
-number_of_args = len(sys.argv)
-
-if number_of_args < 2:
-    print("{} arguments.".format(number_of_args - 1))
-elif number_of_args == 2:
-    print("{} argument:".format(number_of_args -1))
-    print("{}: {}".format(number_of_args -1, sys.argv[1]))
-else:
-    print("{} arguments:".format(number_of_args -1))
-    for i in range(1, number_of_args):
-        print("{}: {}".format(i,sys.argv[i]))
+    number_of_args = len(sys.argv)
+    if number_of_args < 2:
+        print("{} arguments.".format(number_of_args - 1))
+    elif number_of_args == 2:
+        print("{} argument:".format(number_of_args -1))
+        print("{}: {}".format(number_of_args -1, sys.argv[1]))
+    else:
+        print("{} arguments:".format(number_of_args -1))
+        for i in range(1, number_of_args):
+            print("{}: {}".format(i,sys.argv[i]))
