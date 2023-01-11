2bc3c729d9a9a1fc3bf701ad2b243140a3e1bb0b
rnrnshn <olymangue@gmail.com>
2023-01-12 01:10:00 +0200

    add whitespace around operator to fix warnings

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
     if number_of_args < 2:
         print("{} arguments.".format(number_of_args - 1))
     elif number_of_args == 2:
-        print("{} argument:".format(number_of_args -1))
-        print("{}: {}".format(number_of_args -1, sys.argv[1]))
+        print("{} argument:".format(number_of_args - 1))
+        print("{}: {}".format(number_of_args - 1, sys.argv[1]))
     else:
-        print("{} arguments:".format(number_of_args -1))
+        print("{} arguments:".format(number_of_args - 1))
         for i in range(1, number_of_args):
             print("{}: {}".format(i,sys.argv[i]))
