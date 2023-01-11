9912f251f5218878bd2b1b8f49dd177808edd833
rnrnshn <olymangue@gmail.com>
2023-01-12 01:12:18 +0200

    add whitespace around comma

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(number_of_args - 1))
         for i in range(1, number_of_args):
-            print("{}: {}".format(i,sys.argv[i]))
+            print("{}: {}".format(i, sys.argv[i]))
