96fedb1e3bd7d96ce474a7d02d625b0ec684f4a6
rnrnshn <olymangue@gmail.com>
2023-01-11 15:24:57 +0200

    fix the code so that it pass

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import add_0
-
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
